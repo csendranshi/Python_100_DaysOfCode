5fe1ba8d513c332723fc387977312a8b04b76b22
anshika <csendranshi@gmail.com>
2020-11-09 22:06:45 +0530

    band name generator

---
+++ Day_1/bandNameGenerator.py
@@ -0,0 +1,4 @@
+print("Hola from the Band-Name-Creatinator! (PS : the Doof way)")
+city = input("Enter the city you grew up in: ")
+pet = input("Enter the name of your pet: ")
+print("Your Band Name could be:",city,pet)

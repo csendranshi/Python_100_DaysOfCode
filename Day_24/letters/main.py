07c25fb9d7d8d403fc69a582b7cb29c438ce17d0
anshika <csendranshi@gmail.com>
2020-12-07 04:21:59 +0530

    Day 24 code updated

--- Day_24/letters/main.py
+++ Day_24/letters/main.py
@@ -1,15 +1,19 @@
 # TODO: Create a letter using starting_letter.docx
 # for each name in invited_names.txt
 mail_draft = open("Input/Letters/starting_letter.docx")
-draft = mail_draft.read()
+draft = mail_draft.readlines()
+print(draft[0])
 # Replace the [name] placeholder with the actual name.
 names_file = open("Input/Names/invited_names.txt")
 Names = names_file.readlines()
 
 for name in Names:
     send_file = open(f"Output/ReadyToSend/Invite_{name.strip()}.docx",'w')
-    draft.replace('[name]',name.strip())
-    send_file.write(draft)
+    print(name.strip(), type(name.strip()))
+    draft[0] = draft[0].replace(draft[0][5:-1],name.strip())
+    print(draft[0])
+    s=""
+    send_file.write(s.join(draft))
 
 # Save the letters in the folder "ReadyToSend".
 

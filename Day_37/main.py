34c4acd9783011934ef5e2bddbf1c1259fde936a
anshika <csendranshi@gmail.com>
2020-12-26 21:48:54 +0530

    Day 37 code added

--- Day_37/main.py
+++ Day_37/main.py
@@ -1,4 +1,5 @@
 import requests
+from datetime import datetime
 
 USERNAME = "anshikagupta"
 TOKEN = "anshikagupta"
@@ -27,5 +28,34 @@ headers = {
     "X-USER-TOKEN": TOKEN
 }
 
-response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
-print(response)
+# response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
+# print(response)
+
+today = datetime.now()
+#
+# add_value_endpoint = f"{graph_endpoint}/{graph_config['id']}"
+# add_value_config = {
+#     "date":today.strftime("%Y%m%d"),
+#     "quantity":"7",
+#
+# }
+yesterday = datetime(2020,12,25)
+
+add_value_endpoint = f"{graph_endpoint}/{graph_config['id']}"
+add_value_config = {
+    "date":yesterday.strftime("%Y%m%d"),
+    "quantity":input("How many paintings did you make today? "),
+
+}
+
+
+# response = requests.post(url=add_value_endpoint, json=add_value_config, headers=headers)
+# print(response.text)
+
+update_pixel_endpoint = f"{add_value_endpoint}/{today.strftime('%Y%m%d')}"
+update_pixel_config = {
+    "quantity":"5",
+}
+
+response = requests.put(url=update_pixel_endpoint, json=update_pixel_config, headers=headers)
+print(response.text)
